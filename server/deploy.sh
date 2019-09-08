0c8e858a7e40cf28c1063092df62e27385953582
Brian Brown <brbrowngeo@gmail.com>
2019-09-08 11:11:55 -0400

    remove jenkins build files

--- server/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x #echo on
-
-echo "Building package"
-node_modules/@angular/cli/bin/ng build --prod --aot 
-echo "Syncing package"
-rm -rf /var/www/groceries/*
-rsync -a ./dist/groceries/ /var/www/groceries/
--- server/sync.sh
+++
@@ -1,2 +0,0 @@
-# ng build --prod --aot 
-rsync -a ./dist/groceries/ bgeo@159.203.127.52:/var/www/groceries/
